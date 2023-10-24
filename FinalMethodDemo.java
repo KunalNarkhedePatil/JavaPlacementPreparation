ab116edb6dbbb12399789b05436501455005480d
KunalNarkhedePatil <narkhedekunal53@gmail.com>
2023-10-24 13:24:09 +0530

    commit

--- FinalMethodDemo.java
+++ FinalMethodDemo.java
@@ -1,4 +1,33 @@
+//if method is final then it not be override
+class Base
+{
+    public int X;
+
+    public void fun()
+    {
+        System.out.println("Inside Base fun...");
+    }
+    public final void gun()
+    {
+        System.out.println("Inside Base gun...");
+    }
+}
+class Derived extends Base
+{
+    public int Y;
+
+    public void gun()   //can not override because its gun is final in Base class
+    {
+        System.out.println("Inside Derived gun...");
+    }
+}
 public class FinalMethodDemo 
 {
-        
+   public static void main(String[] args) 
+   {
+        Base obj=new Derived();
+
+        obj.fun();
+        //obj.gun();
+   }
 }
